e934137bdf1912da1e83d67b5d8602e16813c950
SerggAidd <you@example.com>
2026-02-19 19:02:33 +0300

    main.tf updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,11 @@
 # Provider init
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.130"
     }
   }
 }
