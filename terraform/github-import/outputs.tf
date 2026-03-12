15ad415d18c263613a9027f294b8ddb9b24fda9b
Linktur <seva.nazmudinov@gmail.com>
2026-03-12 22:30:12 +0300

    Update outputs.tf

--- terraform/github-import/outputs.tf
+++ terraform/github-import/outputs.tf
@@ -1,3 +1,4 @@
 output "repository_full_name" {
-  value = github_repository.course_repo.full_name
+  description = "Full name of the imported GitHub repository"
+  value       = github_repository.course_repo.full_name
 }
