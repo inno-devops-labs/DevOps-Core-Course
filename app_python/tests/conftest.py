2364cc9f8ae28666acd524006dc23aaf6a40c40a
Daniil Mayorov <d.mayorov@innopolis.university>
2026-04-16 20:56:21 +0300

    Add solution to lab 12

--- app_python/tests/conftest.py
+++ app_python/tests/conftest.py
@@ -2,19 +2,20 @@
 import pytest
 import tempfile
 import os
+
+_temp_visits_file = tempfile.NamedTemporaryFile(delete=False)
+_temp_visits_file.close()
+os.environ['VISITS_FILE'] = _temp_visits_file.name
+
 from app_python.app import app
 
 @pytest.fixture
 def client():
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-    temp_file.close()
-    os.environ['VISITS_FILE'] = temp_file.name
-    
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
     
     try:
-        os.unlink(temp_file.name)
+        os.unlink(_temp_visits_file.name)
     except OSError:
         pass
